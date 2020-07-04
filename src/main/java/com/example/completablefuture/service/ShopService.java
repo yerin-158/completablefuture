679ee0c15f909c672e8abd001328ffc45903f4cd
yerin-158 <hanblueblue@gmail.com>
2020-07-04 15:33:41 +0900

    메소드 분리

--- src/main/java/com/example/completablefuture/service/ShopService.java
+++ src/main/java/com/example/completablefuture/service/ShopService.java
@@ -11,24 +11,25 @@ import java.util.*;
 public class ShopService {
 
     public Map<String, Object> getSyncPrice(){
-        Map<String, Object> response = new HashMap<>();
-
         long beforeTime = System.currentTimeMillis();
+        List<Map<Long, Double>> prices = getPrices();
+        long afterTime = System.currentTimeMillis();
 
+        Map<String, Object> response = new HashMap<>();
+        response.put("prices", prices);
+        response.put("executionTime", (afterTime - beforeTime)/1000);
+
+        return response;
+    }
+
+    private List<Map<Long, Double>> getPrices() {
         List<Map<Long, Double>> prices = new ArrayList<>();
         for (int i = 0; i < 10; ++i){
             Shop shop = new Shop(i, RandomUtil.getRandomString(5));
             Double price = getSyncPrice(shop, RandomUtil.getRandomString(3));
             prices.add(Map.of(shop.getId(), price));
         }
-
-        long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
-        long secDiffTime = (afterTime - beforeTime)/1000; //두 시간에 차 계산
-
-        response.put("prices", prices);
-        response.put("executionTime", secDiffTime);
-
-        return response;
+        return prices;
     }
 
     private double getSyncPrice(Shop shop, String product){
