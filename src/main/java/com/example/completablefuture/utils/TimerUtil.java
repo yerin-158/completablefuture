bd502bb23aa6e3ed0d1bd92ec64c1c84c4e263a9
yerin-158 <hanblueblue@gmail.com>
2020-07-04 13:27:49 +0900

    1초를 딜레이하는 함수 추가

---
+++ src/main/java/com/example/completablefuture/utils/TimerUtil.java
@@ -0,0 +1,14 @@
+package com.example.completablefuture.utils;
+
+public class TimerUtil {
+
+    public static void delay() {
+        int delay = 1000;
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
