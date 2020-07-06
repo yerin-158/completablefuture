ad3e1b8796b531992a9a21b29f03526d93fecf4f
yerin-158 <hanblueblue@gmail.com>
2020-07-06 22:35:00 +0900

    cf를 사용하여 request attribute에 접근을 요청하는 3가지 버전의 api를 추가

--- src/main/java/com/example/completablefuture/controller/ShopApiController.java
+++ src/main/java/com/example/completablefuture/controller/ShopApiController.java
@@ -1,6 +1,7 @@
 package com.example.completablefuture.controller;
 
 import com.example.completablefuture.domain.RequestType;
+import com.example.completablefuture.service.HttpContactService;
 import com.example.completablefuture.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,8 @@ public class ShopApiController {
 
     @Autowired
     private ShopService shopService;
+    @Autowired
+    private HttpContactService httpContactService;
 
     @GetMapping("/")
     public ResponseEntity<String> connectTest() {
@@ -25,6 +28,27 @@ public class ShopApiController {
     @GetMapping("/price")
     public ResponseEntity<Map> getPrice(@RequestParam("type") RequestType type) throws ExecutionException, InterruptedException {
         return ResponseEntity.ok(shopService.getPrice(type));
+    }
+
+    @GetMapping("/read")
+    public ResponseEntity<String> readHttpSession() throws ExecutionException, InterruptedException {
+        // cf로 생성된 thread에서 부모 스레드인 request의 정보를 읽을 수 있도록 한다.
+        httpContactService.read();
+        return ResponseEntity.ok("read success!!");
+    }
+
+    @GetMapping("/v2/read")
+    public ResponseEntity<String> readHttpSession2() throws ExecutionException, InterruptedException {
+        // cf로 생성된 thread에서 부모 스레드인 request의 정보를 읽을 수 있도록 한다.
+        httpContactService.readWithLocalVariable();
+        return ResponseEntity.ok("read2 success!!");
+    }
+
+    @GetMapping("/v3/read")
+    public ResponseEntity<String> readHttpSession3() throws ExecutionException, InterruptedException {
+        // cf로 생성된 thread에서 부모 스레드인 request의 정보를 읽을 수 있도록 한다.
+        httpContactService.readByLocalSetting();
+        return ResponseEntity.ok("read3 success!!");
     }
 
 }
