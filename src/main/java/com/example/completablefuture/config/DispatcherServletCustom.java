af4f7eeb2862e76bb3079461cbb6b56c5305a909
yerin-158 <hanblueblue@gmail.com>
2020-07-06 22:27:56 +0900

    request attribute의 상속을 허용하는 디스패처 서블릿을 설정한다.

---
+++ src/main/java/com/example/completablefuture/config/DispatcherServletCustom.java
@@ -0,0 +1,19 @@
+package com.example.completablefuture.config;
+
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+public class DispatcherServletCustom {
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+        dispatcherServlet.setThreadContextInheritable(true);
+        return dispatcherServlet;
+    }
+
+}
